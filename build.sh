3c0b8ccea06b5b120205998f13accd506a1ea514
sivaldo <sivaldovieiradealmeida@gmail.com>
2026-03-24 11:04:04 -0300

    Comando para criar seed_data

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py seed_data  # <--- Adicione esta linha
+#python manage.py seed_data  # <--- Adicione esta linha
 
 python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@exemplo.com', 'Sivaldo123')"
 
